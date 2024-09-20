f3e30e023751893937e4ebaf5f75250a7f9f564d
Kat Brandt <joyfulkat11@gmail.com>
2024-09-20 11:26:57 -0600

    Add fetch call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
 import Ideas from './Ideas';
 import Form from './Form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const dummyIdeas = [
-    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
-  ]
+  // const dummyIdeas = [
+  //   { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+  //   { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+  //   { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+  // ]
+  const [ideas, setIdeas] = useState([]);
 
-  const [ideas, setIdeas] = useState(dummyIdeas);
+  // async function getIdeas() {
+  //   const url = "http://localhost:3001/api/v1/ideas"
+  //   try {
+  //     const response = await fetch(url);
+  //     if (!response.ok) {
+  //       throw new Error(`Response status: ${response.status}`)
+  //     }
+  //     const fetchedIdeas = await response.json();
+  //     setIdeas([... ideas, fetchedIdeas]);
+  //     console.log('success!', json);
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // }
+
+  function getIdeas() {
+    fetch('http://localhost:3001/api/v1/ideas')
+      .then(response => response.json())
+      .then(data => setIdeas([...ideas, ...data]))
+      .catch(error => console.log(error.message))
+  }
+
+  useEffect(() => {
+    getIdeas();
+  }, [])
 
   function addIdea(newIdea) {
     setIdeas([...ideas, newIdea])
